e2c7fa9bb11593d270fae2bf5e80572f27b464b8
Caterina Fosser <catefosser@gmail.com>
2023-08-31 12:48:14 -0300

    entrega y actualizacion P1

--- Practica1.R
+++ Practica1.R
@@ -78,3 +78,56 @@ a + b * a -c
 
 
 #Ejercicio 6----
+# Programa para calcular la inversa de un numero
+rm(list=ls())
+a<-readlin("Ingrese un numero: ")
+x<-as.numeric(a)
+inversa_x<-(1/x)
+inversa_x
+
+
+#Ejercicio 7-----
+#a - Coriolis
+lat<-as.numeric(readline("Ingrese la latitud:"))
+Coriolis=2*(7.27*exp(-5))*sin(lat)
+print(paste("El parámetro de coriolis para la latitud ingresada es",Coriolis))
+
+#b 
+temp_f<-as.numeric(readline("Ingrese la temperatura (°F):"))
+temp_c<-(temp_f - 32)*5/9
+#terminar jeje
+
+#Ejercicio 8----
+#a
+anio<-readline("anio (YYYY):")
+mes<-readline("mes (MM):")
+dia<-readline("dia (DD):")
+fecha=paste(anio,mes,dia,sep=".")
+
+#b
+fecha<-readline("ingrese la fecha (YYYY-MM-DD):")
+dia<-substr(fecha,9,10)
+mes<-substr(fecha,6,7)
+anio<-substr(fecha,1,4)
+print(paste("La fecha ingresada es",dia,"del",mes,"de",anio))
+
+#c
+nombre<-readline("Ingrese un nombre:")
+primer_letra<-substr(nombre,1,1)
+ult_letra<-substr(nombre,nchar(nombre),nchar(nombre))
+
+#Ejercicio 9------
+nombre<-readline("Ingresa tu nombre:")
+edad<-as.numeric(readline("Ingresa tu edad:"))
+print(paste(nombre,"va a tener",edad+7,"años en 2030"))
+#Ejercicio 10----
+rm(list=ls())
+#Pido los datos
+nombre<-readline("Ingrese su nombre:")
+apellido<-readline("Ingrese su apellido:")
+libreta<-readline("Ingrese su libreta universitaria (NNNN/AA):")
+mat_aprob<-as.numeric(readline("Ingrese la cantidad de materias aprobadas:"))
+puesto<-substr(libreta,1,nchar(libreta)-3) #extraigo el valor de N
+anio<-as.numeric(substr(libreta,nchar(libreta)-1,nchar(libreta))) #extraigo el valor de AA
+mat_falt<-(20-mat_aprob) #calculo las materias faltantes
+print(paste("Lx estudiante",nombre,apellido,"se inscribio como estudiante de Exactas en el puesto",puesto,"en el año",paste("20",anio,sep=""),"y debe aprobar",mat_falt,"materias para obtener el titulo de grado"))
